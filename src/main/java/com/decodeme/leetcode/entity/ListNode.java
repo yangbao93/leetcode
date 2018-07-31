d348baa5e0d4c8b6ce2148f8cc133310f074bde5
yangbao <yangbao@baidu.com>
2018-07-31 18:13:32 +0800

    链表entity

---
+++ src/main/java/com/decodeme/leetcode/entity/ListNode.java
@@ -0,0 +1,15 @@
+package com.decodeme.leetcode.entity;
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int x) { val = x; }
+
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+}
