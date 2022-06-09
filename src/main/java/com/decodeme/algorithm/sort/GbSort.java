076fde797979b8a4e36dabda20bd7b228763b829
yangbao <875674770@qq.com>
2022-06-09 16:13:47 +0800

    提交归并算法

---
+++ src/main/java/com/decodeme/algorithm/sort/GbSort.java
@@ -0,0 +1,42 @@
+package com.decodeme.algorithm.sort;
+
+import com.alibaba.fastjson.JSON;
+import com.decodeme.util.ArrayUtil;
+
+import java.util.Arrays;
+
+public class GbSort {
+
+    public int[] gbSort(int[] nums) {
+        if (nums.length < 2) {
+            return nums;
+        }
+        int mid = nums.length / 2;
+        int[] left = Arrays.copyOfRange(nums, 0, mid);
+        int[] right = Arrays.copyOfRange(nums, mid, nums.length);
+        return merge(gbSort(left), gbSort(right));
+    }
+
+    private int[] merge(int[] left, int[] right) {
+        int[] result = new int[left.length + right.length];
+        for (int idx = 0, i = 0, j = 0; idx < result.length; idx++) {
+            if (i >= left.length) {
+                result[idx] = right[j++];
+            } else if (j >= right.length) {
+                result[idx] = left[i++];
+            } else if (left[i] > right[j]) {
+                result[idx] = right[j++];
+            } else {
+                result[idx] = left[i++];
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        GbSort sort = new GbSort();
+        int[] ints = ArrayUtil.toArrays(1, 3, 2, 7, 6, 4);
+        System.out.println(JSON.toJSONString(sort.gbSort(ints)));
+
+    }
+}
